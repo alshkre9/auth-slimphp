6b4a5c03fe3e4e2f7b5fad063246c6f1b62b7556
alshkre9 <polyex9@gmail.com>
2023-08-19 05:32:28 +0200

    rename class and file

--- src/app/Controllers/MainController.php
+++ src/app/Controllers/MainController.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Controllers\Controller;
 
-class HomeController extends Controller
+class MainController extends Controller
 {
     public function index(Request $request, Response $response)
     {
